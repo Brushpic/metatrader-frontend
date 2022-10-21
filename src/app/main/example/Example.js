1733145b0834282335b22dc127e2d12daed03325
whale-watching <chenchrissh@gmail.com>
2022-10-20 20:02:58 -0700

    first change

--- src/app/main/example/Example.js
+++ src/app/main/example/Example.js
@@ -16,7 +16,7 @@ function SimpleFullWidthSample() {
       }}
       header={
         <div className="p-24">
-          <h4>Header</h4>
+          <h4>Head</h4>
         </div>
       }
       contentToolbar={
